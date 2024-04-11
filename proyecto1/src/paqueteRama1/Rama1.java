613186f2d5ccae56b20cc88f8a0b8eb266a490ca
mañana <maana@A1-21>
2024-04-11 10:24:13 +0200

    Confirmame esta rama

---
+++ proyecto1/src/paqueteRama1/Rama1.java
@@ -0,0 +1,12 @@
+package paqueteRama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("La puta rama de kk");
+		
+	}
+
+}
